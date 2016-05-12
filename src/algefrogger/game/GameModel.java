285d19f58a8f212ecdbfaa3efd49828b5c41af3b
Karl <37496339+aquarial@users.noreply.github.com>
2016-05-12 13:27:19 -0500

    Adding some skeleton to the project

--- src/algefrogger/game/GameModel.java
+++ src/algefrogger/game/GameModel.java
@@ -2,19 +2,15 @@ package algefrogger.game;
 
 public class GameModel {
     private LevelState state;
-    
-    /*  _ _ _ _ _ _ _ _ _ _ _ _ _ 
-     * |3 | | | 8 | | | 4 | | | 2|
-     * |= = = =   = = = =   = =  |
-     * | e e   e e   e e   e e   | - 2 long e-turtle
-     * |=   = = = = =   = = = =  | - 5 long log (ln148)
-     * |  = = =   = = =   = = =  | - 3 long log (ln20)     
-     * |p p   p p p   p p p   p p| - 3 long pi-turtles
-     * |                         | - plain row
-     * |------------7------3-----| - car lane
-     * |---2---4--------6--------| - car lane
-     * |---------5---------1-----| - car lane
-     * |----3-----4-----8--------| - car lane
-     * |_ _ _ _ _ _ X _ _ _ _ _ _| - starting row
-     */
+
+    public GameModel() {
+        state = new LevelState();
+    }
+
+    void movePlayerLeft() {
+        // if (state.playerpos() < 40){
+        // return;
+        // }
+        // state.moveplayerby(-40, 0);
+    }
 }
--- src/algefrogger/game/LevelState.java
+++ src/algefrogger/game/LevelState.java
@@ -2,4 +2,24 @@ package algefrogger.game;
 
 public class LevelState {
 
+    //@formatter:off
+    /*  _ _ _ _ _ _ _ _ _ _ _ _ _ 
+     * |3 | | | 8 | | | 4 | | | 2|
+     * |= = = =   = = = =   = =  |
+     * | e e   e e   e e   e e   | - 2 long e-turtle
+     * |=   = = = = =   = = = =  | - 5 long log (ln148)
+     * |  = = =   = = =   = = =  | - 3 long log (ln20)     
+     * |p p   p p p   p p p   p p| - 3 long pi-turtles
+     * |                         | - plain row
+     * |------------7------3-----| - car lane
+     * |---2---4--------6--------| - car lane
+     * |---------5---------1-----| - car lane
+     * |----3-----4-----8--------| - car lane
+     * |_ _ _ _ _ _ X _ _ _ _ _ _| - starting row
+     */
+    //@formatter:on
+
+    public LevelState() {
+        // put cars and things in place
+    }
 }
