16edddc249cb96562d3e9ac7f1a270165442a9e1
Karl <37496339+aquarial@users.noreply.github.com>
2016-05-17 12:58:51 -0500

    use gamemodel IEntities

--- src/algefrogger/game/GameModel.java
+++ src/algefrogger/game/GameModel.java
@@ -1,11 +1,20 @@
 package algefrogger.game;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import algefrogger.game.entity.IEntity;
+
 //Game logic
 public class GameModel {
     private LevelState state;
 
     public GameModel() {
         state = new LevelState();
+    }
+
+    public List<IEntity> getAllIEntities() {
+        return new ArrayList<>();
     }
 
     void movePlayerLeft() {
